94a406b612d9877a44a6143a021235d1bb58a18e
manmakris <manolis.makris@datawise.ai>
2022-12-03 03:45:21 +0200

    to spitaki sou mesa

--- src/components/GUI/Avatar.jsx
+++ src/components/GUI/Avatar.jsx
@@ -1,7 +1,22 @@
-const Avatar = () =>{
-  return <div>
+import {Box, Stack, Typography} from "@mui/material";
+import AvatarImage from '../../assets/images/avatar.png'
+import MapImage from '../../assets/icons/mapClosed.png'
 
-  </div>
+const Avatar = ({nickName='Tzimani',location='Mutilini',conquers=15}) => {
+  return <Box flex alignItems={'center'} style={{width: 250, minHeight: 300, height: 'auto'}}>
+    <img src={AvatarImage} style={{borderRadius: '50%', border: '10px solid #987548', width: 180, height: 180}}
+         alt={'players avatar'}/>
+    <Box style={{width: 160, minHeight: 200, padding: 10, backgroundColor: '#2f1805', marginTop: -80, marginLeft: 12}}>
+      <Stack pt={10} alignItems={'center'} direction={'column'}>
+        <Typography variant={'h5'} color={'#fa990e'} fontFamily={'gameFont'} fontWeight={'800'}>{nickName}</Typography>
+        <Typography color={'#ccd33a'} fontFamily={'gameFont'} fontWeight={'600'}>{location}</Typography>
+        <Stack direction={'row'} mt={4} alignItems={'center'} spacing={2}>
+          <img src={MapImage} height={15} width={15}/>
+          <Typography fontFamily={'gameFont'} color={'white'}>15 {conquers}</Typography>
+        </Stack>
+      </Stack>
+    </Box>
+  </Box>
 }
 
 
