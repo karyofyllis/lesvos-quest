f8faefa6846bca8e4cea4b90581e0f9b307ff5b6
giorgos.karyofyllis <giorgos.karyofyllis@datawise.ai>
2022-12-03 03:49:00 +0200

    Merge

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import Playground from "./pages/playground";
-
+import "./App.css";
+import { Box, CssBaseline } from "@mui/material";
+import QuestMap from "./components/QuestMap";
+import Avatar from "./components/GUI/Avatar";
 
 function App() {
   return (
-    <BrowserRouter>
-    <Routes>
-      <Route path="/playground" element={<Playground />}/>
-    </Routes>
-    </BrowserRouter>
+    <Box>
+      <CssBaseline />
+      <Box sx={{ height: "100vh", width: "100%" }}>
+        <QuestMap />
+        <Box style={{ position: "fixed", top: 20, right: 0, zIndex: 999 }}>
+          <Avatar />
+        </Box>
+      </Box>
+    </Box>
   );
 }
 
