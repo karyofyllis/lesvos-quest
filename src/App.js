571096662da02d34f53837c9131ac996dcd61333
giorgos.karyofyllis <giorgos.karyofyllis@datawise.ai>
2022-12-03 13:09:34 +0200

    Add music to game

--- src/App.js
+++ src/App.js
@@ -3,8 +3,17 @@ import { Box, CssBaseline } from "@mui/material";
 import QuestMap from "./components/QuestMap";
 import Avatar from "./components/GUI/Avatar";
 import Navigation from "./components/Navigation";
+import useSound from "use-sound";
+import track from "./assets/audio/desert.mp3";
+import {useEffect} from "react";
 
 function App() {
+  const [play] = useSound(track);
+
+  useEffect(() => {
+    play()
+  }, [play])
+
   return (
     <Box>
       <CssBaseline />
