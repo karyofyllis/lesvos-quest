699d77b21e2f2d4f1668d37931fa3d84505afb04
giorgos.karyofyllis <giorgos.karyofyllis@datawise.ai>
2022-12-04 10:20:28 +0200

    Add quiz dialog

---
+++ src/components/@core/PapyrosDialog.js
@@ -0,0 +1,38 @@
+import * as React from "react";
+import Dialog from "@mui/material/Dialog";
+import { Box, Stack, Typography } from "@mui/material";
+
+const bg = require("../../assets/images/papurousDialog.png");
+
+export default function PapyrosDialog(props) {
+  const { open, handleClose, title } = props;
+
+  return (
+    <Dialog fullWidth maxWidth={"md"} onClose={handleClose} open={open} PaperComponent={Box}>
+      <Stack
+        p={5.5}
+        rowGap={4}
+        sx={{
+          margin: "auto",
+          height: 650,
+          width: 650,
+          backgroundImage: `url(${bg})`,
+          backgroundRepeat: `no-repeat`,
+          backgroundSize: "cover",
+        }}
+      >
+        <Typography
+          className={"shadow"}
+          sx={{ color: "#ad3c17", fontWeight: "bold", textAlign: "center" }}
+          variant={"h6"}
+        >
+          {title}
+        </Typography>
+
+        <Stack px={10} rowGap={4} mt={2}>
+          {props.children}
+        </Stack>
+      </Stack>
+    </Dialog>
+  );
+}
--- src/components/QuizDialog/index.js
+++ src/components/QuizDialog/index.js
@@ -1,56 +1,136 @@
 import * as React from "react";
-import {useEffect, useState} from "react";
-import Dialog from "@mui/material/Dialog";
-import {Stack, Typography} from "@mui/material";
-import ButtonBased from "../ButtonBased";
+import { useEffect, useState } from "react";
+import { Card, CardActionArea, Stack, Typography } from "@mui/material";
+import PapyrosDialog from "../@core/PapyrosDialog";
+import yes from "../../assets/audio/mixkit-males-yes-victory-2012.wav";
+import victory from "../../assets/audio/mixkit-animated-small-group-applause-523.wav";
+import defeat from "../../assets/audio/mixkit-crowd-disappointment-long-boo-463.wav";
+import useSound from "use-sound";
 
-function QuizBody({questions, index, handleAnswer}) {
+function QuizBody({
+  questions,
+  index,
+  handleAnswer,
+  correctAnswers,
+  wrongAnswers,
+}) {
   const question = questions && questions[index];
 
   if (!question) {
-    return <Typography>Results</Typography>;
+    return (
+      <Stack rowGap={2}>
+        <Typography
+          variant={"h4"}
+          sx={{ color: "#ad3c17", fontWeight: "bold", textAlign: "center" }}
+        >
+          ΑΠΟΤΕΛΕΣΜΑ
+        </Typography>
+
+        <Typography
+          sx={{ color: "#ad3c17", fontWeight: "bold", textAlign: "center" }}
+        >
+          {correctAnswers === 0
+            ? "Δεν κατάφερες να βρείς καμία σωστή απάντηση!"
+            : `Απάντησες σωστά στις ${correctAnswers} απο τις ${questions.length} ερωτήσεις!`}
+        </Typography>
+      </Stack>
+    );
   }
 
   return (
-    <Stack>
+    <Stack rowGap={1}>
+      <Typography
+        sx={{ color: "#ad3c17", fontWeight: "bold", textAlign: "center" }}
+      >{`${index + 1}/${questions.length}`}</Typography>
+      <Typography
+        sx={{
+          color: "#ad3c17",
+          fontWeight: "bold",
+          mb: 2,
+          textAlign: "center",
+        }}
+        variant={"h6"}
+      >
+        {question.label}
+      </Typography>
       {question.options.map((option) => {
         return (
-          <ButtonBased
-            image={"buttonLeftMenu.png"}
+          <Card
+            className={"btn"}
             key={option.value}
             width={"100%"}
             onClick={() => handleAnswer(option)}
-            text={option.value}
           >
-          </ButtonBased>
+            <CardActionArea sx={{ p: 2 }} disableRipple>
+              <Typography
+                className={"shadow"}
+                sx={{ color: "white", fontWeight: "bold" }}
+              >
+                {option.value}
+              </Typography>
+            </CardActionArea>
+          </Card>
         );
       })}
     </Stack>
   );
 }
 
-export default function QuizDialog({open, handleClose, monument}) {
+export default function QuizDialog({ open, handleClose, monument }) {
   const [index, setIndex] = useState(0);
+  const [answered, setAnswered] = useState([]);
 
-  const handleAnswer = () => {
+  const [playDefeat, defeatExposer] = useSound(defeat);
+  const [playVictory, victoryExposer] = useSound(victory);
+  const [playYes, yesExposer] = useSound(yes);
+
+  const handleAnswer = (answer) => {
+    setAnswered((prevState) => {
+      const newAnswers = [...prevState, answer];
+
+      const correctAnswers = newAnswers.filter((x) => x.correct).length;
+
+      if (newAnswers.length === monument.questions.length) {
+        if (correctAnswers === newAnswers.length) {
+          playYes();
+          playVictory();
+        } else {
+          playDefeat();
+        }
+      }
+
+      return newAnswers;
+    });
     setIndex(index + 1);
   };
 
   useEffect(() => {
-    if (open) setIndex(0);
+    if (open) {
+      setIndex(0);
+      setAnswered([]);
+    }
   }, [open]);
 
-  return (
-    <Dialog onClose={handleClose} open={open}>
-      <Stack p={2} justifyContent={"center"}>
-        <Typography variant={"h5"}>{monument.label}</Typography>
+  useEffect(() => {
+    if (!open) {
+      victoryExposer.stop();
+      yesExposer.stop();
+      defeatExposer.stop();
+    }
+  }, [defeatExposer, open, victoryExposer, yesExposer]);
 
-        <QuizBody
-          questions={monument.questions}
-          index={index}
-          handleAnswer={handleAnswer}
-        />
-      </Stack>
-    </Dialog>
+  const correctAnswers = answered.filter((x) => x.correct).length;
+  const wrongAnswers = answered.filter((x) => !x.correct).length;
+
+  return (
+    <PapyrosDialog handleClose={handleClose} open={open} title={monument.label}>
+      <QuizBody
+        questions={monument.questions}
+        index={index}
+        handleAnswer={handleAnswer}
+        correctAnswers={correctAnswers}
+        wrongAnswers={wrongAnswers}
+      />
+    </PapyrosDialog>
   );
 }
