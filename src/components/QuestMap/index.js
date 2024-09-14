8194de3283019e79a607798129df86817d3b5ea0
giorgos.karyofyllis <karyofyllisgiorgos@gmail.com>
2024-09-14 22:36:16 +0300

    Fix map. Update packages and resolve vulnerabilities.

--- src/components/QuestMap/index.js
+++ src/components/QuestMap/index.js
@@ -90,7 +90,7 @@ function QuestMap({ isPlaying, togglePlayer }) {
     >
       <TileLayer
         attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}"
+        url="https://tiles.stadiamaps.com/tiles/stamen_watercolor/{z}/{x}/{y}.jpg"
         ext={"jpg"}
       />
       {/*<LocationMarker />*/}
